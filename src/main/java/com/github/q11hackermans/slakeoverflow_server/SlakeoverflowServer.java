c55ee51f34555e48630d070818b49cdcdb9ff414
jandie1505 <jande1234567890@gmail.com>
2022-04-26 11:29:47 +0200

    json strings comment

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
@@ -2,6 +2,6 @@ package com.github.q11hackermans.slakeoverflow_server;
 
 public class SlakeoverflowServer {
     public static void main(String[] args) {
-        // HIER GEHTS LOS :)
+        // HIER GEHTS LOS :) {"cmd":"tick","fields":[[0,1,0],[],[]]}  {"cmd":"auth","username":"vollidiot123"} {"cmd":"auth2","sizex":100,"sizey":100} {"cmd":"auth3"}
     }
 }
