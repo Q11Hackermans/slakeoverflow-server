161aebbec1baf3e61dbbe013c9c9a5a540750c0f
Ersatzhase <102618574+Ersatzhase@users.noreply.github.com>
2022-04-29 12:13:18 +0200

    start server

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
@@ -1,7 +1,21 @@
 package com.github.q11hackermans.slakeoverflow_server;
 
+import net.jandie1505.connectionmanager.server.CMSServer;
+
+import java.io.IOException;
+
 public class SlakeoverflowServer {
-    public static void main(String[] args) {
-        // HIER GEHTS LOS :) {"cmd":"tick","fields":[[0,1,0],[],[]]}  {"cmd":"auth","username":"vollidiot123"} {"cmd":"auth2","sizex":100,"sizey":100} {"cmd":"auth3"}
+
+    private CMSServer connectionhandler;
+
+
+    public SlakeoverflowServer() throws IOException {
+        this.connectionhandler = new CMSServer(55555);
+
+    }
+    // HIER GEHTS LOS :) {"cmd":"tick","fields":[[0,1,0],[],[]]}  {"cmd":"auth","username":"vollidiot123"} {"cmd":"auth2","sizex":100,"sizey":100} {"cmd":"auth3"}
+
+    public static void main(String[] args) throws IOException {
+        new SlakeoverflowServer();
     }
 }
