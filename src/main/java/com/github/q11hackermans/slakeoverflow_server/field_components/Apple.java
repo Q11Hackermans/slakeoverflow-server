bc011fb28dd974b2637b942d4a7ee7713093f060
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2022-04-30 21:40:17 +0200

    deleted not required classes

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/field_components/Apple.java
+++
@@ -1,5 +0,0 @@
-package com.github.q11hackermans.slakeoverflow_server.field_components;
-
-public class Apple extends FieldObject {
-
-}
--- src/main/java/com/github/q11hackermans/slakeoverflow_server/field_components/Void.java
+++
@@ -1,4 +0,0 @@
-package com.github.q11hackermans.slakeoverflow_server.field_components;
-
-public class Void extends FieldObject {
-}
