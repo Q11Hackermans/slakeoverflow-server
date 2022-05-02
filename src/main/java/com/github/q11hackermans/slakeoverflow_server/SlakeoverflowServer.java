727a0dfa0d3b779f9d236e5c544c3813281dd97b
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2022-05-02 21:27:05 +0200

    added start arguments and ascii signature

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_server/SlakeoverflowServer.java
@@ -12,9 +12,8 @@ import net.jandie1505.connectionmanager.utilities.dataiostreamhandler.DataIOType
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
@@ -44,8 +43,12 @@ public class SlakeoverflowServer {
         // SET SERVER (RUN ALWAYS FIRST)
         server = this;
 
+        // WELCOME MESSAGE
+        System.out.println(getASCIISignature());
+
         // CONSOLE
         this.logger = new ConsoleLogger();
+        this.logger.info("INIT", "Server init");
         this.console = new ServerConsole(this.logger);
         this.console.start();
 
@@ -186,10 +189,58 @@ public class SlakeoverflowServer {
      */
     // HIER GEHTS LOS :) {"cmd":"tick","fields":[[0,1,0],[],[]]}  {"cmd":"auth","username":"vollidiot123"} {"cmd":"auth2","sizex":100,"sizey":100} {"cmd":"auth3"}
     public static void main(String[] args) throws IOException {
+        System.out.println("Slakeoverflow Server by Q11-Hackermans (https://github.com/Q11Hackermans)");
+
+        int waitTime = 3;
+        Map<String, String> startArguments = new HashMap<>();
+        try {
+            for(String arg : args) {
+                if(arg.startsWith("-")) {
+                    arg = arg.replace("-", "");
+                    try {
+                        String[] argument = arg.split("=");
+                        startArguments.put(argument[0], argument[1]);
+                    } catch(Exception e) {
+                        System.out.println("Incorrect start argument: " + arg);
+                        waitTime = 10;
+                    }
+                } else {
+                    System.out.println("Wrong start argument format: " + arg);
+                    waitTime = 10;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error with start arguments. Starting with default arguments...");
+            waitTime = 30;
+        }
+
+        System.out.println("Starting server in " + waitTime + " seconds...");
+        try {
+            TimeUnit.SECONDS.sleep(waitTime);
+        } catch(Exception ignored) {}
+
         new SlakeoverflowServer();
     }
 
     public static SlakeoverflowServer getServer() {
         return server;
+    }
+
+    private static String getASCIISignature() {
+        return "\n" +
+                "  _____ _               _  ________ ______      ________ _____  ______ _      ______          __\n" +
+                " / ____| |        /\\   | |/ /  ____/ __ \\ \\    / /  ____|  __ \\|  ____| |    / __ \\ \\        / /\n" +
+                "| (___ | |       /  \\  | ' /| |__ | |  | \\ \\  / /| |__  | |__) | |__  | |   | |  | \\ \\  /\\  / / \n" +
+                " \\___ \\| |      / /\\ \\ |  < |  __|| |  | |\\ \\/ / |  __| |  _  /|  __| | |   | |  | |\\ \\/  \\/ /  \n" +
+                " ____) | |____ / ____ \\| . \\| |___| |__| | \\  /  | |____| | \\ \\| |    | |___| |__| | \\  /\\  /   \n" +
+                "|_____/|______/_/    \\_\\_|\\_\\______\\____/   \\/   |______|_|  \\_\\_|    |______\\____/   \\/  \\/    \n" +
+                "  _____                          \n" +
+                " / ____|                         \n" +
+                "| (___   ___ _ ____   _____ _ __ \n" +
+                " \\___ \\ / _ \\ '__\\ \\ / / _ \\ '__|\n" +
+                " ____) |  __/ |   \\ V /  __/ |   \n" +
+                "|_____/ \\___|_|    \\_/ \\___|_|   \n" +
+                "\n" +
+                "Slakeoverflow Server by Q11-Hackermans (https://github.com/Q11Hackermans)\n";
     }
 }
