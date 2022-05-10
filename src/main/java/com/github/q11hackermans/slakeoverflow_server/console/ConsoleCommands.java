cc873c5f9443dc9b33f7a23ee7182c732370393e
jandie1505 <jande1234567890@gmail.com>
2022-05-10 11:27:17 +0200

    started config command console

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/console/ConsoleCommands.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_server/console/ConsoleCommands.java
@@ -14,8 +14,33 @@ public class ConsoleCommands {
                 case "stop":
                     SlakeoverflowServer.getServer().stop();
                     return "Shutting down";
+                case "config":
+                    return configCommand(cmd);
             }
         }
         return "";
+    }
+
+    private static String configCommand(String[] cmd) {
+        if(cmd.length >= 2) {
+            if(cmd[1].equalsIgnoreCase("set")) {
+                if(cmd.length == 4) {
+                    if(cmd[3].equalsIgnoreCase("")) {
+
+                    }
+                } else {
+                    return "Run command without arguments for help";
+                }
+            } else if(cmd[1].equalsIgnoreCase("get")) {
+
+            } else if(cmd[1].equalsIgnoreCase("list")) {
+
+            } else {
+                return "Run command without arguments for help";
+            }
+        } else {
+            return "Command usage:\nconfig set <OPTION> <VALUE>\nconfig get <OPTION>\nconfig list\n";
+        }
+        return "damit der fehler verschwindet..."; // TO BE REMOVED
     }
 }
