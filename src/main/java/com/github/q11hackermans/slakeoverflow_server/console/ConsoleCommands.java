0d80ca28e7ac7894a77cb6e6989e15dd5b45e1aa
jandie1505 <jande1234567890@gmail.com>
2022-05-11 17:44:19 +0200

    config command

--- src/main/java/com/github/q11hackermans/slakeoverflow_server/console/ConsoleCommands.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_server/console/ConsoleCommands.java
@@ -22,66 +22,86 @@ public class ConsoleCommands {
     }
 
     private static String configCommand(String[] cmd) {
-        if(cmd.length >= 2) {
-            if(cmd[1].equalsIgnoreCase("set")) {
+        if (cmd.length >= 2) {
+            if (cmd[1].equalsIgnoreCase("set")) {
                 if(cmd.length == 4) {
-                    if(cmd[2].equalsIgnoreCase("port")) {
+                    if (cmd[2].equalsIgnoreCase("port")) {
                         return "Cannot set port while the server is running!";
-                    } else if(cmd[2].equalsIgnoreCase("whitelist")) {
+                    } else if (cmd[2].equalsIgnoreCase("whitelist")) {
                         SlakeoverflowServer.getServer().getConfigManager().getConfig().setWhitelist(Boolean.parseBoolean(cmd[3]));
                         return "Updated value whitelist to " + cmd[3];
-                    } else if(cmd[2].equalsIgnoreCase("slots")) {
+                    } else if (cmd[2].equalsIgnoreCase("slots")) {
                         try {
                             int slots = Integer.parseInt(cmd[3]);
-                            if(slots > 0) {
+                            if (slots > 0) {
                                 SlakeoverflowServer.getServer().getConfigManager().getConfig().setSlots(slots);
                                 return "Updated value slots to " + slots;
                             } else {
                                 return "You can only set a positive int value";
                             }
-                        } catch(NumberFormatException e) {
+                        } catch (NumberFormatException e) {
                             return "You can only set a positive int value";
                         }
-                    } else if(cmd[2].equalsIgnoreCase("min_food_value")) {
+                    } else if (cmd[2].equalsIgnoreCase("min_food_value")) {
                         try {
                             int minFoodValue = Integer.parseInt(cmd[3]);
-                            if(minFoodValue >= 1 && minFoodValue <= 10) {
+                            if (minFoodValue >= 1 && minFoodValue <= 10) {
                                 SlakeoverflowServer.getServer().getConfigManager().getConfig().setMinFoodValue(minFoodValue);
                                 return "Updated value min_food_value to " + minFoodValue;
                             } else {
                                 return "You can only set an int value in range 1-10";
                             }
-                        } catch(NumberFormatException e) {
+                        } catch (NumberFormatException e) {
                             return "You can only set an int value in range 1-10";
                         }
-                    } else if(cmd[2].equalsIgnoreCase("max_food_value")) {
+                    } else if (cmd[2].equalsIgnoreCase("max_food_value")) {
                         try {
                             int maxFoodValue = Integer.parseInt(cmd[3]);
-                            if(maxFoodValue >= 1 && maxFoodValue <= 10) {
+                            if (maxFoodValue >= 1 && maxFoodValue <= 10) {
                                 SlakeoverflowServer.getServer().getConfigManager().getConfig().setMaxFoodValue(maxFoodValue);
                                 return "Updated value max_food_value to " + maxFoodValue;
                             } else {
                                 return "You can only set an int value in range 1-10";
                             }
-                        } catch(NumberFormatException e) {
+                        } catch (NumberFormatException e) {
                             return "You can only set an int value in range 1-10";
                         }
                     } else {
-                        return "Config option not found";
+                        return "Unknown config option";
                     }
                 } else {
                     return "Run command without arguments for help";
                 }
-            } else if(cmd[1].equalsIgnoreCase("get")) {
-
-            } else if(cmd[1].equalsIgnoreCase("list")) {
-
+            } else if (cmd[1].equalsIgnoreCase("get")) {
+                if(cmd.length == 3) {
+                    if(cmd[2].equalsIgnoreCase("port")) {
+                        return "Value port: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getPort();
+                    } else if(cmd[2].equalsIgnoreCase("slots")) {
+                        return "Value slots: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getSlots();
+                    } else if(cmd[2].equalsIgnoreCase("whitelist")) {
+                        return "Value whitelist: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().isWhitelist();
+                    } else if(cmd[2].equalsIgnoreCase("min_food_value")) {
+                        return "Value min_food_value: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getMinFoodValue();
+                    } else if(cmd[2].equalsIgnoreCase("max_food_value")) {
+                        return "Value max_food_value: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getMaxFoodValue();
+                    } else {
+                        return "Unknown config option";
+                    }
+                } else {
+                    return "Run command without arguments for help";
+                }
+            } else if (cmd[1].equalsIgnoreCase("list")) {
+                return "ALL CONFIG OPTIONS:\n" +
+                        "port: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getPort() + "\n" +
+                        "slots: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getSlots() + "\n" +
+                        "whitelist: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().isWhitelist() + "\n" +
+                        "min_food_value: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getMinFoodValue() + "\n" +
+                        "max_food_value: " + SlakeoverflowServer.getServer().getConfigManager().getConfig().getMaxFoodValue() + "\n";
             } else {
                 return "Run command without arguments for help";
             }
         } else {
             return "Command usage:\nconfig set <OPTION> <VALUE>\nconfig get <OPTION>\nconfig list\n";
         }
-        return "damit der fehler verschwindet..."; // TO BE REMOVED
     }
 }
